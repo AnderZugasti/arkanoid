7e90b636340a09d9e7b3d2fd92bb14b69f5edef4
ander <ander.zugasti@ikasle.egibide.org>
2020-05-29 17:26:07 +0200

    terminado

--- arkanoid/Assets/Scripts/Puntuaciones/Ranking.cs
+++ arkanoid/Assets/Scripts/Puntuaciones/Ranking.cs
@@ -16,27 +16,28 @@ public class Ranking : MonoBehaviour
     private puntuacionFinal jugador;
     private int score;
     public GameObject puntosJugador;
-    public Text nick;
+    public TextMeshProUGUI nick;
     List<string> nicks = new List<string>();
     private bool repe = false;
     public GameObject textRepe;
-    public GameObject  guardar;
+    public GameObject guardar;
     public GameObject inputField;
     // Start is called before the first frame update
     void Start()
     {
         jugador = FindObjectOfType<puntuacionFinal>();
         score = jugador.puntos;
-        puntosJugador.GetComponent<TextMeshProUGUI>().text =$" {score} puntos";
+        puntosJugador.GetComponent<TextMeshProUGUI>().text = $" {score} puntos";
         textRepe.SetActive(false);
-            
-        
+
+
 
         db = FirebaseFirestore.GetInstance(FirebaseApp.Create());
         CollectionReference jugadoresRef = db.Collection("jugadores");
-        Query query = jugadoresRef.OrderByDescending("puntos");
+        Query query = jugadoresRef.OrderByDescending("puntos").Limit(5);
         ListenerRegistration listener = query.Listen(snapshot =>
         {
+
             string top5 = "TOP 5 \n\n";
             var cont = 1;
             foreach (DocumentSnapshot documentSnapshot in snapshot.Documents)
@@ -44,7 +45,7 @@ public class Ranking : MonoBehaviour
                 Dictionary<string, object> jugador = documentSnapshot.ToDictionary();
                 top5 += $"{cont}-{documentSnapshot.Id}..........{jugador["puntos"]}\n";
                 cont += 1;
-                nicks.Add(documentSnapshot.Id);
+                nicks.Add(documentSnapshot.Id.ToString());
             }
             marcador.GetComponent<TextMeshProUGUI>().text = top5;
         });
@@ -54,42 +55,43 @@ public class Ranking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void guardarNombre()
     {
-        nick = GameObject.Find("inputNick").GetComponent<Text>();
+        repe = false;
+        nick = GameObject.Find("inputNick").GetComponent<TextMeshProUGUI>();
 
         Debug.Log(nick);
-       
-            foreach (var nic in nicks)
-            {
+
+        foreach (var nic in nicks)
+        {
             Debug.Log("entra en el for each");
             if (nick.text.Equals(nic))
-                {
-                    repe = true;
-                }
-                else
-                {
-                Debug.Log("entrapara meter datos");
-                DocumentReference docref = db.Collection("jugadores").Document(nick.text);
-                var datos = new Dictionary<string,object> {
+            {
+                repe = true;
+            }
+           
+        }
+        if (repe)
+        {
+            textRepe.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("entrapara meter datos");
+            DocumentReference docref = db.Collection("jugadores").Document(nick.text);
+            var datos = new Dictionary<string, object> {
                         {"puntos", score }
                     };
-                    
-                    docref.SetAsync(datos);
-                    guardar.SetActive(false);
 
-                }
+            docref.SetAsync(datos);
+            guardar.SetActive(false);
+            textRepe.SetActive(false);
 
-
-            }
-            if (repe)
-            {
-                textRepe.SetActive(true);
-            }
+        }
 
 
-        
+
     }
 }
